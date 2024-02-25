2dfe8d4a674b8cdab5a7cca0c94a1fffd4cabb9a
Sanjay Kapilesh Sridhar <ufoundsanjay@gmail.com>
2024-02-25 14:11:35 +0000

    Changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * Filters out users below the age of 18 from the given array.
  *
- * @param {Array} users - An array of user objects with 'age' property.
+ * @param {Array} users - An array of user objects.
  * @returns {Array} - A new array containing only users above or equal to 18 years of age.
  */
 function filterUsers(users) {
